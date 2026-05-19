266887268b1398904da9385eddf978c6061f3171
Saminakalwar <saminakalwar9@gmail.com>
2026-05-19 15:25:08 +0500

    Add default route for production

--- backend/server.js
+++ backend/server.js
@@ -79,7 +79,9 @@ app.use((req, res, next) => {
 app.use(expressLogger);
 
 //Routes
-
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/auth", passwordRoutes);
